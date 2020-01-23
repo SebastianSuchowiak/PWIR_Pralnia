55bf9d62f591aeb5916073da460c4cfb3427bc09
SebastianSuchowiak <suchowiak.sebastian@gmail.com>
2020-01-23 20:50:00 +0100

    Build and run instructions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PWIR_Pralnia
+To build "make:all()" in eshell
+
+to run "erl -pa ./ebin -pa . -s main main" in shell
+
--- src/main_test.erl
+++ src/main_test.erl
@@ -15,10 +15,10 @@ test_machine_exists_test_() ->
   Machines = main:create_washing_machines([5, 6, 7, 8]),
   ets:new(laundry, [named_table, public, set]),
   ets:insert(laundry, {machines, Machines}),
-  [?_assertEqual(machine_communication:machine_exists(1)),
+  [?_assert(machine_communication:machine_exists(1)),
     ?_assert(machine_communication:machine_exists(0)),
     ?_assert(machine_communication:machine_exists(1)),
     ?_assert(machine_communication:machine_exists(1)),
-    ?_assert(machine_communication:machine_exists(1)),].
+    ?_assert(machine_communication:machine_exists(1))].
 
 
